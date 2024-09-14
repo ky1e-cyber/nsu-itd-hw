99bb9ebda550c5a810dab36c7450fc0114d264c3
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2024-09-14 16:21:39 +0700

    cleanup and final solution for 11

--- C/pack1/task11.c
+++ C/pack1/task11.c
@@ -1,46 +1,27 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <math.h>
 
-static inline uint64_t count_cubes2(uint32_t n) {
+// using inline to eliminate unnecessary
+// function call
+
+static inline uint64_t count_cubes(uint32_t n) {
+  uint32_t cbr_n = (uint32_t)cbrtf(n);
+
   uint64_t count = 0;
-
-  for (uint32_t i = 1; i <= n; i++) {
-    for (uint32_t k = i; k <= (n / ((uint64_t)i * i)); k++) {
-      count += (n / ((uint64_t)i * k)) - i; // REDO
+  for (uint32_t i = 1; i <= cbr_n ; i++) {
+    for (uint32_t j = i; j * j <= n / i; j++) {
+      uint32_t c = (n / ((uint64_t)i * j));
+      count += c >= j ? c - j + 1 : 0;
     }
   }
 
   return count;
 }
 
-// uint64_t count_cubes(uint32_t n) {
-//   uint64_t count = 0;
-//   for (uint32_t i = 1; i <= n; i++) {
-//     uint32_t j = i;
-//     while ((i * j) <= n) {
-//       uint64_t c = (n / ((uint64_t)j * i));
-//       if (c >= (j - 1)) {
-//         count += c - (j - 1);
-//       } else {
-//         break;
-//       }
-//       j++;
-//     }
-//   }
-
-//   return count;
-// }
-
 int main() {
-  FILE* inp = fopen("input.txt", "r");
-  FILE* out = fopen("output.txt", "w");
-
   uint32_t n;
-  fscanf(inp, "%u", &n);
-  fprintf(out, "%llu", count_cubes2(n));
-
-  fclose(inp);
-  fclose(out);
-
+  scanf("%u", &n);
+  printf("%llu", count_cubes(n));
   return 0;
 }
