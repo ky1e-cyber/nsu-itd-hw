1d83f8ef79365ab664180a9e0322913837218ee5
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2024-09-26 23:37:22 +0700

    wip on pack2 haskell

--- Haskell/pack2/src/Main.hs
+++ Haskell/pack2/src/Main.hs
@@ -28,14 +28,47 @@ harmsum :: Natural -> Rational
 harmsum n =
   sum [1 / toRational i | i <- [1 .. n]]
 
-solver :: (Double -> Double) -> Segment -> Double -> Double
-solver f seg eps = undefined
+sign :: Double -> Integer
+sign x = if x < 0.0 then -1 else (if x > 0.0 then 1 else 0)
+
+solver :: (Double -> Double) -> Segment -> Double -> Maybe Double
+solver f seg eps
+  | start seg > end seg = Nothing
+  | sign (start seg) == sign (end seg) = Nothing
+  | eps < 0.0 = Nothing
+  | otherwise =
+      let solverConfirmed f seg eps =
+            let x = (start seg + end seg) / 2
+                y = f x
+             in if y <= eps
+                  then x
+                  else
+                    if y > 0.0
+                      then solverConfirmed f (Segment {start = (start seg), end = x}) eps
+                      else solverConfirmed f (Segment {start = x, end = (end seg)}) eps
+       in Just (solverConfirmed f seg eps)
 
 arctanTaylorSteps :: Double -> Natural -> Double
-arctanTaylorSteps x n = undefined
+arctanTaylorSteps x n =
+  let seriesTail (acc :: Double) (i :: Natural) =
+        if i == n
+          then acc
+          else
+            let sign = (-1) ^^ (i + 1)
+                denom = (2 * i - 1)
+             in seriesTail (acc + sign * ((x ^^ denom) / fromIntegral denom)) (i + 1)
+   in seriesTail x 1
 
 arctanTaylorEps :: Double -> Double -> Double
-arctanTaylorEps x eps = undefined
+arctanTaylorEps x eps =
+  let seriesTail (acc :: Double) (i :: Natural) =
+        let sign = (-1) ^^ (i + 1)
+            denom = 2 * i - 1
+            next = acc + sign * ((x ^^ denom) / fromIntegral denom)
+         in if abs (next - acc) <= eps
+              then next
+              else seriesTail next (i + 1)
+   in seriesTail x 1
 
 main :: IO ()
 main = print (fib 35)
