03318604444c0b379e55281d0a68f3d8c98e8552
sxrose <53537708+ky1e-cyber@users.noreply.github.com>
2024-12-17 17:27:11 +0700

    task 1

--- C/pack7/task1.c
+++ C/pack7/task1.c
@@ -1,121 +1,103 @@
 #include <assert.h>
 #include <inttypes.h>
-#include <stdbool.h>
-#include <stddef.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define STRVAL_SIZE 8
 
-#define VECTOR_INIT_SIZE 16
-#define VECTOR_GROW_FACTOR 2
-
-typedef struct __attribute__((aligned(8))) {
-  size_t capacity;
-  size_t length;
-} vector_header_t;
-
-typedef vector_header_t* vector_ptr_t;
-
-size_t vector_get_length(vector_ptr_t vec);
-
-size_t vector_get_capacity(vector_ptr_t vec);
-
-void vector_init(vector_ptr_t vec);
-
-void* vector_basepointer(vector_ptr_t vec);
-
-#define vector_make(T) vector_make_sized(sizeof(T))
-
-void vector_free(vector_ptr_t vec);
-
-vector_ptr_t vector_make_sized(size_t elem_size);
-
-vector_ptr_t vector_grow_sized(vector_ptr_t vec, size_t elem_size);
-
-vector_ptr_t vector_append_fake(vector_ptr_t vec, size_t elem_size);
-
-#define vector_grow(T, vec) vector_grow_sized(vec, sizeof(T))
-
-#define vector_append(T, vec, elem)                                \
-  ({                                                               \
-    vector_ptr_t _grown = vector_append_fake(vec, sizeof(T));      \
-    if (_grown != NULL) {                                          \
-      ((T*)vector_basepointer(_grown))[_grown->length - 1] = elem; \
-    }                                                              \
-    _grown;                                                        \
-  })
-
-#define vector_get_array(T, vec) (T*)vector_basepointer(vec)
-
-#define vector_get(T, vec, ind) (vector_get_array(T, vec))[ind]
-
-vector_ptr_t vector_make_sized(size_t elem_size) {
-  vector_ptr_t mem = (vector_ptr_t)malloc(sizeof(vector_header_t) +
-                                          VECTOR_INIT_SIZE * elem_size);
-  if (mem == NULL)
-    return NULL;
-  vector_init(mem);
-
-  return mem;
-}
-
-vector_ptr_t vector_grow_sized(vector_ptr_t vec, size_t elem_size) {
-  size_t new_cap = vec->capacity * VECTOR_GROW_FACTOR;
-  assert(vec->capacity * elem_size < new_cap * elem_size);
-  vector_ptr_t new_mem = (vector_ptr_t)realloc(vec, new_cap * elem_size);
-  if (new_mem == NULL)
-    return NULL;
-  new_mem->capacity = new_cap;
-
-  return new_mem;
-}
-
-vector_ptr_t vector_append_fake(vector_ptr_t vec, size_t elem_size) {
-  if (vec->capacity <= vec->length) {
-    vec = vector_grow_sized(vec, elem_size);
-    if (vec == NULL)
-      return NULL;
-  }
-
-  vec->length++;
-
-  return vec;
-}
-
-size_t vector_get_length(vector_ptr_t vec) {
-  return vec->length;
-}
-
-size_t vector_get_capacity(vector_ptr_t vec) {
-  return vec->capacity;
-}
-
-void vector_init(vector_ptr_t vec) {
-  vec->capacity = VECTOR_INIT_SIZE;
-  vec->length = 0;
-}
-
-void* vector_basepointer(vector_ptr_t vec) {
-  return (void*)(vec + 1);
-}
-
-void vector_free(vector_ptr_t vec) {
-  free(vec);
-}
-
-bool vector_isempty(vector_ptr_t vec) {
-  return vector_get_length(vec) == 0;
-}
-
 typedef struct {
-  uint64_t key;
+  uint32_t key;
   char value[STRVAL_SIZE];
 } item_t;
 
+static inline long long radix_sort_get_key(long long key, unsigned int radix) {
+  return (key / radix) % 10;
+}
+
+// radix : [1, 10, 100 ...]
+void radix_sort_counting(item_t* arr,
+                         item_t* dest,
+                         long long size,
+                         unsigned int digit) {
+  unsigned int radix = pow(10, digit - 1);
+
+  unsigned int cnt[11] = {0};
+
+  for (long long i = 0; i < size; i++)
+    cnt[radix_sort_get_key(arr[i].key, radix)]++;
+
+  for (int i = 1; i < 11; i++)
+    cnt[i] += cnt[i - 1];
+
+  for (long long i = size - 1; i >= 0; i--) {
+    long long key = radix_sort_get_key(arr[i].key, radix);
+    dest[cnt[key] - 1] = arr[i];
+    cnt[key]--;
+  }
+}
+
+static inline void buf_swap(item_t** buf1, item_t** buf2) {
+  item_t* tmp = *buf1;
+  *buf1 = *buf2;
+  *buf2 = tmp;
+}
+
+static inline void buf_cpy(item_t* dst, item_t* src, size_t size) {
+  // pls do memcpy
+  for (size_t i = 0; i < size; i++)
+    dst[i] = src[i];
+}
+
+item_t* radix_sort(item_t* arr, size_t size) {
+  item_t* buf1 = arr;
+  item_t* buf2 = (item_t*)malloc(size * sizeof(item_t));
+  if (buf2 == NULL)
+    return NULL;
+
+  unsigned int max_width = 1;
+  for (size_t i = 0; i < size; i++) {
+    unsigned int width = log10f(arr[i].key) + 1;
+    if (width > max_width)
+      max_width = width;
+  }
+
+  for (unsigned int digit = 1; digit <= max_width; digit++) {
+    radix_sort_counting(buf1, buf2, size, digit);
+    buf_swap(&buf1, &buf2);
+  }
+
+  if (max_width % 2 != 0) {
+    buf_cpy(buf2, buf1, size);
+    buf_swap(&buf1, &buf2);
+  }
+
+  free(buf2);
+
+  return arr;
+}
+
+static inline void read_item(item_t* it) {
+  scanf("%u", &it->key);
+  fgetc(stdin);  // eat space
+  scanf("%s", it->value);
+}
+
 int main() {
   size_t n;
+  scanf("%lu", &n);
+
+  item_t* arr = (item_t*)malloc(n * sizeof(item_t));
+
+  for (size_t i = 0; i < n; i++)
+    read_item(arr + i);
+
+  radix_sort(arr, n);
+
+  for (size_t i = 0; i < n; i++)
+    printf("%u %s\n", arr[i].key, arr[i].value);
+
+  free(arr);
 
   return 0;
 }
